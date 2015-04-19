22e1b4de4b8f70279f1b5f875b6f9213f65def31
benshrimpton <benjaminshrimpton@yahoo.co.uk>
2015-04-19 16:51:37 -0400

    Added RS

--- wp-content/themes/sarahappleby/index.php
+++ wp-content/themes/sarahappleby/index.php
@@ -1,6 +1,24 @@
 <?php get_header(); ?>
 
 
+<section id="homepage-gallery" class="royalSlider rsDefault">
+<?php query_posts('post_type=homeslider, & posts_per_page=1'); ?>
+
+<?php  if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+<?php
+$images = get_field('images');
+$image = $images[0];
+?>
+
+<?php foreach( $images as $image ): ?>
+<a href="<?php echo $image['sizes']['large']; ?>" class="rsImg"/></a>
+<?php endforeach; ?>
+
+<?php endwhile; endif;  ?>
+<?php wp_reset_query(); ?>
+</section>
+
 <section class="container">
 	<div class="row">
 		<div class="col-md-8">
--- wp-content/themes/sarahappleby/js/main.js
+++ wp-content/themes/sarahappleby/js/main.js
@@ -1,4 +1,58 @@
 // @codekit-prepend "bootstrap.min.js"; 
+// @codekit-prepend "royalslider/jquery.royalslider.min.js"; 
+
+// Royal slider for homepage
+
+function goRoyalHomepage() {
+	
+    if ($('#homepage-gallery').length) {
+    
+    			$(this).remove();
+	    		var $royalSlider = $('#homepage-gallery');
+	            $royalSlider.royalSlider({
+	            	addActiveClass: true,
+	            	imageScaleMode: 'fill',
+	            	//slidesOrientation: 'vertical',
+	                //controlNavigation: 'none',
+	                //imageScalePadding: 40,
+	                slidesSpacing: 0,
+	                navigateByClick: false,
+	                numImagesToPreload: 2,
+	                //arrowsNav: false,
+	                arrowsNavAutoHide: true,
+	                arrowsNavHideOnTouch: true,
+	                keyboardNavEnabled: true,
+	                fadeinLoadedSlide: true,
+	                globalCaption: false,
+	                globalCaptionInside: false,
+	                transitionSpeed: 300,
+	                sliderDrag: false,
+	                autoPlay: {
+	                    // autoplay options go gere
+	                    pauseOnHover: false,
+	                    enabled: true,
+	                    delay: 3000
+	                }
+	            });
+	            var slider = $royalSlider.data('royalSlider');
+	            slider.ev.on('rsAfterSlideChange', function(event) {
+		            	// triggers after slide change
+		            	
+		            	var tone = $('.rsActiveSlide > .slide').data('tone');
+		            	console.log("changed", tone);
+		            	$('.header').attr('id',tone);
+    			});
+				slider.slides[0].holder.on('rsAfterContentSet', function() {
+				// fires when first slide content is loaded and added to DOM
+				 		var tone = $('.rsActiveSlide > .slide').data('tone');
+		            	console.log(tone);
+		            	$('.header').attr('id',tone);
+				});
+				
+       }//end if
+  
+} //end function
 
 
-alert("hello")
+
+goRoyalHomepage(); 
