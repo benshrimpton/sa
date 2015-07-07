a86d975601f1b9a4a4ee96fbd217190c78ab990e
benshrimpton <benjaminshrimpton@yahoo.co.uk>
2015-07-06 21:32:46 -0400

    Sidebar menu

--- wp-content/themes/sarahappleby/page-instagram-feed.php
+++ wp-content/themes/sarahappleby/page-instagram-feed.php
@@ -2,7 +2,10 @@
 
 <section class="container instagram-page-wrapper">
   <header class="page-header text-center">
-    <a href="https://instagram.com/oksarahappleby/" target="_blank" class="read-more"><span>&#xe100;</span>#sarahappleby</a>
+    <a href="https://instagram.com/oksarahappleby/" target="_blank" class="read-more">@oksarahappleby </a> &nbsp; &nbsp;
+    <a href="https://instagram.com/explore/tags/sarahappleby/" target="_blank" class="read-more">#sarahappleby </a>
+
+      <!-- <span>&#xe100;</span>#sarahappleby</a>-->
   </header>
   <div class="row" id="instagram-page"><!--- ajax loaded --></div>
   <footer class="text-center load-more-wrap">
