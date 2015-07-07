07d711ac100a266f324f80d0aaeba9f28a29478e
benshrimpton <benjaminshrimpton@yahoo.co.uk>
2015-07-06 21:18:09 -0400

    Add png favicon

--- wp-content/themes/sarahappleby/header.php
+++ wp-content/themes/sarahappleby/header.php
@@ -13,6 +13,7 @@
   ?></title>
   <meta name="description" content="">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="icon" type="image/png" href="/favicon.png" />
 
   <link rel="apple-touch-icon" href="/apple-touch-icon.png">
   <link rel="profile" href="http://gmpg.org/xfn/11" />
